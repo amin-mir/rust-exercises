ef6874b53a6affbe6d372a6b8bdb4a3d1aa7fe92
Amin Mir <aminmir326@gmail.com>
2022-03-27 20:29:47 +0300

    Add deref exercise

---
+++ deref-enum-to-u8-slc/src/main.rs
@@ -0,0 +1,33 @@
+use std::sync::Arc;
+use core::ops::Deref;
+
+fn main() {
+    let b1 = Block::Owned(vec![1, 3, 4]);
+    let a1 = &*b1;
+    println!("{:?}", a1);
+
+    let b2 = Block::Cached(Arc::new(vec![5, 6, 7]));
+    let a2 = &b2[..];
+    println!("{:?}", a2);
+}
+
+enum Block {
+    Owned(Vec<u8>),
+    Cached(Arc<Vec<u8>>),
+}
+
+impl Deref for Block {
+    type Target = [u8];
+    fn deref(&self) -> &[u8] {
+        match self {
+            Self::Owned(v) => {
+                println!("Yea it is a vector");
+                v.as_ref()
+            }
+            Self::Cached(a) => {
+                println!("Wow it was an Arc this time");
+                a.as_ref()
+            }
+        }
+    }
+}
