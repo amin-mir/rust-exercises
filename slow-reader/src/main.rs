640d94b6342a87d181c764cb4781648e4b8dfec2
Amin Mir <aminmir326@gmail.com>
2022-10-25 22:31:06 +0300

    Implement SlowReader for AsyncReader trait

---
+++ slow-reader/src/main.rs
@@ -0,0 +1,23 @@
+use std::io;
+use std::time::Instant;
+use tokio::fs::File;
+use tokio::io::AsyncReadExt;
+use pin_utils::pin_mut;
+
+mod slow_reader;
+use slow_reader::SlowReader;
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let now = Instant::now();
+
+    let f = File::open("/dev/urandom").await?;
+    let sr = SlowReader::new(f);
+    pin_mut!(sr);
+
+    let mut buf = [0; 256 * 1024]; // 256KiB
+    let n = sr.read_exact(&mut buf).await?;
+
+    println!("read byte count: {}, in {:?}", n, now.elapsed());
+    Ok(())
+}
---
+++ slow-reader/src/slow_reader.rs
@@ -0,0 +1,52 @@
+use tokio::io::{AsyncRead, ReadBuf};
+use tokio::time::{self, Duration, Sleep, Instant};
+use std::io::Result;
+use std::future::{Future, Pending};
+use std::pin::Pin;
+use std::task::{self, Context, Poll};
+
+pub struct SlowReader<R> {
+    sleep: Sleep,
+    reader: R,
+}
+
+impl<R> SlowReader<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            sleep: time::sleep(Duration::from_millis(200)),
+            reader,
+        }
+    }
+}
+
+impl<R> AsyncRead for SlowReader<R>
+where
+    R: AsyncRead + Unpin,
+{
+    fn poll_read(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &mut ReadBuf<'_>,
+    ) -> task::Poll<Result<()>> {
+        let (mut sleep, reader) = unsafe {
+            let this = self.get_unchecked_mut();
+            (
+                Pin::new_unchecked(&mut this.sleep),
+                &mut this.reader,
+            )
+        };
+
+        match sleep.as_mut().poll(cx) {
+            Poll::Pending => Poll::Pending,
+            Poll::Ready(_) => {
+                let reader = Pin::new(reader);
+                if let Poll::Ready(res) = reader.poll_read(cx, buf) {
+                    Poll::Ready(res)
+                } else {
+                    sleep.reset(Instant::now() + Duration::from_millis(25));
+                    Poll::Pending
+                }
+            }
+        }
+    }
+}
